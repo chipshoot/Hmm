cea9d3a095ceef78cf9b9f8df3a8546598e0e071
Chaoyang Fang <fchy@outlook.com>
2024-09-13 23:50:21 -0400

    added author and catalog validate for note

--- src/Hmm.Core/DefaultManager/Validator/NoteValidator.cs
+++ src/Hmm.Core/DefaultManager/Validator/NoteValidator.cs
@@ -5,22 +5,36 @@ using Hmm.Utility.Validation;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Hmm.Utility.Dal.Query;
 
 namespace Hmm.Core.DefaultManager.Validator
 {
     public class NoteValidator : ValidatorBase<HmmNote>
     {
-        private readonly IHmmNoteManager _noteManager;
+        private readonly IEntityLookup _lookup;
 
-        public NoteValidator(IHmmNoteManager noteManager)
+        public NoteValidator(IEntityLookup lookup)
         {
-            Guard.Against<ArgumentNullException>(noteManager == null, nameof(noteManager));
-            _noteManager = noteManager;
+            Guard.Against<ArgumentNullException>(lookup == null, nameof(lookup));
+            _lookup = lookup;
 
             RuleFor(n => n.Subject).NotNull().Length(1, 1000);
+            RuleFor(n => n.Author).NotNull().MustAsync(AuthorExists).WithMessage("Cannot find author of the note");
             RuleFor(n => n.Author).NotNull().MustAsync(AuthorNotChanged).WithMessage("Cannot update note's author");
-            RuleFor(n => n.Description).Length(1, 1000);
+            RuleFor(n => n.Catalog).NotNull().MustAsync(NoteCatalogExists).WithMessage("Cannot find note's catalog");
+            RuleFor(n => n.Description).Length(0, 1000);
             RuleFor(n => n.Catalog).NotNull();
+        }
+
+        private async Task<bool> AuthorExists(Author author, CancellationToken cancellationToken)
+        {
+            if (author == null)
+            {
+                return false;
+            }
+
+            var savedAuthor = await _lookup.GetEntityAsync<AuthorDao>(author.Id);
+            return savedAuthor != null;
         }
 
         private async Task<bool> AuthorNotChanged(HmmNote note, Author author, CancellationToken cancellationToken)
@@ -30,7 +44,7 @@ namespace Hmm.Core.DefaultManager.Validator
                 return false;
             }
 
-            var savedNote = await _noteManager.GetNoteByIdAsync(note.Id);
+            var savedNote = await _lookup.GetEntityAsync<HmmNoteDao>(note.Id);
 
             // create new user, make sure account name is unique
             var authorId = author.Id;
@@ -40,6 +54,17 @@ namespace Hmm.Core.DefaultManager.Validator
             }
 
             return savedNote.Author.Id == authorId;
+        }
+
+        private async Task<bool> NoteCatalogExists(NoteCatalog catalog, CancellationToken cancellationToken)
+        {
+            if (catalog == null)
+            {
+                return false;
+            }
+
+            var savedCatalog = await _lookup.GetEntityAsync<NoteCatalogDao>(catalog.Id);
+            return savedCatalog != null;
         }
     }
 }
