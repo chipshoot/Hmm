3cd8e458dd222aa5c4d50731d4d91313576a2a6d
Chaoyang Fang <fchy@outlook.com>
2025-03-23 17:05:03 -0400

    code reformat

--- src/Hmm.Idp/Pages/Grants/ViewModel.cs
+++ src/Hmm.Idp/Pages/Grants/ViewModel.cs
@@ -8,12 +8,20 @@ public class ViewModel
 public class GrantViewModel
 {
     public string ClientId { get; set; }
+
     public string ClientName { get; set; }
+
     public string ClientUrl { get; set; }
+
     public string ClientLogoUrl { get; set; }
+
     public string Description { get; set; }
+
     public DateTime Created { get; set; }
+
     public DateTime? Expires { get; set; }
+
     public IEnumerable<string> IdentityGrantNames { get; set; }
+
     public IEnumerable<string> ApiGrantNames { get; set; }
 }
